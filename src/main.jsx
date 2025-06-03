37b46e02244d77dfa46bcc263d521bdf28a73c3b
asd731004 <61454851+asd731004@users.noreply.github.com>
2025-06-04 01:43:12 +0800

    Add files via upload

--- src/main.jsx
+++ src/main.jsx
@@ -1,1 +1,10 @@
-// main.jsx
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import './index.css'; // 引入 Tailwind CSS
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+);
